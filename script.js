8ea0d5f82b29f34d626c2d3f6b712be4b1b45537
Eric-Obeng <obengeric.3958@gmail.com>
2024-06-18 11:50:16 +0000

    Person object created with name, age and greet properties

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 "use strict";
 
+const Person = {
+  name: "Eric Obeng",
+  age: 27,
+  greet: function () {
+    console.log(
+      `Hello, my name is ${this.name} and I'm ${this.age} years old.`
+    );
+  },
+};
+
+Person.greet();
