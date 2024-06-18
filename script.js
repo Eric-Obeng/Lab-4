f834435de66221e7e66cb7b8b8beff75e886fe48
Eric-Obeng <obengeric.3958@gmail.com>
2024-06-18 15:32:35 +0000

    Using the call, apply(), and bind()

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ const Person = {
 };
 
 Person.greet();
+
+// Demonstrating or experimenting call(), apply() and bind
+// A new objected is created because when the call(), apply(), bind() is applied on the Person object, the value of the "this" will not be seen practically because it is pointing to the same object
+
+const newPerson = {
+  name: "Psalmist Praise",
+  age: 30,
+};
+
+// using call()
+Person.greet.call(newPerson);
+
+// using apply
+Person.greet.apply(newPerson);
+
+// using bind
+const bindObject = Person.greet.bind(newPerson);
+bindObject();
